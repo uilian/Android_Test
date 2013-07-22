6189c05cc6007812027ad92c8d1c8b4d6b331dbb
uilian <uilian@gmail.com>
2013-07-22 17:28:52 -0300

    Initial commit

---
+++ src/br/com/k19_sample/MainActivity.java
@@ -0,0 +1,37 @@
+package br.com.k19_sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_main);
+		setContentView(R.layout.intents_main);
+		
+		Button nextButton = (Button) findViewById(R.id.next_screen);
+		
+		nextButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+}
---
+++ src/br/com/k19_sample/SecondActivity.java
@@ -0,0 +1,16 @@
+package br.com.k19_sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.intents_second);
+		
+	}
+
+}
